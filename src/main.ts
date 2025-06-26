66bbdcde44a49cb840f6f81bdec7e7ea8fe886fe
alex <alexandersazonov0@gmail.com>
2025-06-26 19:16:49 +0300

    fix cors problem

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.use('/metrics', cors());
-
   const corsOrigin = (process.env.CORS_ORIGIN || '*').split(',');
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (corsOrigin.includes('*')) {
-        callback(null, true);
-      } else if (!origin || corsOrigin.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
+
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: (origin, callback) => {
+        if (
+          corsOrigin.includes('*') ||
+          !origin ||
+          corsOrigin.includes(origin)
+        ) {
+          callback(null, true);
+        } else {
+          callback(new Error(`CORS policy: ${origin} not allowed`));
+        }
+      },
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization'],
     },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT || 3000);
+  console.log(`🚀 Server ready at ${process.env.PORT || 3000}`);
 }
 bootstrap();
